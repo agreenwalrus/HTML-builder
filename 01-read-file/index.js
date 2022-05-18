77bc1c295282797433c6869443e9f0f4837ccec6
Yuliya Runova <jul.runova@gmail.com>
2022-05-18 14:47:57 +0400

    feat: tast 1 is complete

---
+++ 01-read-file/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+const reader = fs.createReadStream(filePath);
+
+
+reader.on('data', (chunk) => {
+    console.log('' + chunk);
+});
+
+reader.on('error', (err) => {
+    console.log(err.stack);
+})
